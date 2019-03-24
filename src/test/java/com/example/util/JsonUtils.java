a24aaf5b496ca3ae926e40d220d9f1b85b44563b
yumgh <yuming.h@qq.com>
2019-03-24 22:58:24 +0800

    测试从类路径下加载文件

--- src/test/java/com/example/util/JsonUtils.java
+++ src/test/java/com/example/util/JsonUtils.java
@@ -1,9 +1,12 @@
 package com.example.util;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * @author yuming
@@ -19,5 +22,25 @@ public class JsonUtils {
             e.printStackTrace();
         }
         return jsonStr;
+    }
+
+    @Test
+    public void pathTest() {
+        // 获得同级目录 /D:/workspace/ideaworkspace/test-demo/test-demo/target/test-classes/com/example/util/
+        URL resource = getClass().getResource("");
+        // 使用斜杆从根目录加载
+        // URL resource = getClass().getResource("/user_data.sql");
+        System.out.println("resource.getPath() = " + resource.getPath());
+        File file = new File(resource.getPath());
+        System.out.println("file.getName() = " + file.getName());
+    }
+
+    @Test
+    public void pathTest2() {
+        // 获得根目录,不能使用斜杆
+        URL resource = getClass().getClassLoader().getResource("user_data.sql");
+        System.out.println("resource.getPath() = " + resource.getPath());
+        File file = new File(resource.getPath());
+        System.out.println("file.getName() = " + file.getAbsolutePath());
     }
 }
