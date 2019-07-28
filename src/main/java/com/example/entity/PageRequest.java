28451598ab6b702c70d2350d1e0c3f9d3978bab2
kuangtao <906885821@qq.com>
2019-07-28 10:49:01 +0800

    新增pageRequest类

---
+++ src/main/java/com/example/entity/PageRequest.java
@@ -0,0 +1,23 @@
+package com.example.entity;
+
+public class PageRequest {
+    private int startPage;
+    private int endPage;
+
+
+    public int getStartPage() {
+        return startPage;
+    }
+
+    public void setStartPage(int startPage) {
+        this.startPage = startPage;
+    }
+
+    public int getEndPage() {
+        return endPage;
+    }
+
+    public void setEndPage(int endPage) {
+        this.endPage = endPage;
+    }
+}
