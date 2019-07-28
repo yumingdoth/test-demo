7fe3181cad59de2a9a6a2600b839d92a0c474e0c
yumgh <yuming.h@qq.com>
2019-07-28 10:51:45 +0800

    新建测试类

---
+++ src/test/java/com/example/git/Foo.java
@@ -0,0 +1,19 @@
+package com.example.git;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ming
+ * @date 2019-07-28
+ */
+public class Foo {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void barTest() {
+        logger.info("第一次提交");
+    }
+}
